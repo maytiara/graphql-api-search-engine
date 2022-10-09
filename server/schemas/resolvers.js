dd392eb6caf5a8cec3970646fb48d4876287ccee
May Carandang <maytiara@gmail.com>
2022-10-10 00:06:20 +0800

    defined me (typeDefs) query functionality to retrieve the logged in user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,11 +4,24 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async () => {
-      return User.find();
-    }
+    
+    // me (typeDefs)
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
   },
 
+  
+  Mutation: {
+
+    //
+    login: async (parent, { email, password }) => {
+    },
+  },
 };
 
 module.exports = resolvers;

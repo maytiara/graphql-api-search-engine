95973d9c51bbe8122f4a2db00974735ef292c8ba
May Carandang <maytiara@gmail.com>
2022-10-10 03:14:51 +0800

    revised syntax in one of the arguments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     
     // me (typeDefs)
     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-    me: async (parent, arg, context) => {
+    me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
       }

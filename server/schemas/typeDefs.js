e003b46fd022f5d88276067539c9f5ce2965237e
May Carandang <maytiara@gmail.com>
2022-10-09 20:29:12 +0800

    Added the saveBook field with required parameters

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -27,6 +27,12 @@ const typeDefs = gql`
     
     # this accepts username, email & pwd as (params) then returns Auth (type)
     addUser(username: String!, email: String!, password: String!): Auth
+
+    # this accepts authors, desc, title, image, link & bookId(ID) as (params) then returns User (type)
+    saveBook(authors: [String]!, description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
+
+    # this accepts bookId(ID) as (params) then returns User (type)
+    removeBook(bookId: ID!): User
   }
 
 `;

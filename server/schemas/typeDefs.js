d1b2a1cec70d05a0a74a20bc620fd99e3f164f06
May Carandang <maytiara@gmail.com>
2022-10-09 19:51:16 +0800

    added different types for TypeDefs schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  
+  # fields required for search engine book
+  type Query {
+
+  }
+
+  type Mutation {
+    
+  }
+
+  type User {
+
+  }
+
+  type Book {
+
+  }
+
+  type Auth {
+    
+  }
+`;
+
+module.exports = typeDefs;

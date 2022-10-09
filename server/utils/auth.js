8b55e26ae3ba0f3dfeaa005e8022a45324fa5fec
May Carandang <maytiara@gmail.com>
2022-10-09 23:37:38 +0800

    modified authMiddleware by creating function to authenticate the routes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,35 +1,41 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken'); // stateless jwt
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // ({ req }) == incoming http req
+  authMiddleware: function ({ req }) {
+
+    // allows token to be sent via req.body, req.query, or headers
+    // this is ${token} in client>src>App.js
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
+    // .split (split the readable obj to token)
+    // .pop (gets the last string in an array)
+    // .trim by getting the result value from .pop => this resulted to one line of string
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
+    // verify token and add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // this returns the data in graphQL
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
@@ -37,3 +43,5 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+// check the server > resolver.js  to check the authentication statement

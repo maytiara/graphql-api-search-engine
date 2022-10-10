99d224704dce3895d0781ab67c93747216f057fd
May Carandang <maytiara@gmail.com>
2022-10-11 00:43:03 +0800

    modified filename

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -5,7 +5,7 @@ const {
   saveBook,
   deleteBook,
   login,
-} = require('../../controllers/user-controller');
+} = require('../../controllers/userController');
 
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
